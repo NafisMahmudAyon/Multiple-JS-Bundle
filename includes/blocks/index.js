67eb0511ad7d371f0e694c170eb5e56e926bdac9
unknown <nafismahmudayon@gmail.com>
2024-09-02 13:21:05 +0600

    update

--- includes/blocks/index.js
+++
@@ -1,5 +0,0 @@
-import { setupAccordion } from "./accordion-nested/front-scripts";
-import { setupBackToTop } from "./back-to-top/front-scripts";
-
-setupAccordion()
-setupBackToTop()
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { setupAccordion } from "./file1";
+import { setupBackToTop } from "./file2";
+
+setupAccordion()
+setupBackToTop()
